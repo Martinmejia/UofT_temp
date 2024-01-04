5ca071a702221aff59efe952df74dc10a594f6d2
tinmar <lm.mejiamendoza@utoronto.ca>
2024-01-04 11:13:00 -0500

    Homework_1 building_software

---
+++ building_software/Hello_world.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Say Hello')
+
+parser.add_argument('name')
+#parser.add_argument('--repeats', '-r', type=int, default=1, help='Repeats')
+parser.add_argument('-r', type=int, default=1)
+args = parser.parse_args()
+
+ 
+
+for _ in range(args.r):
+
+  print('Hello ' + args.name + '!')
---
+++ building_software/README.md
@@ -0,0 +1,4 @@
+This folder contains:
+	- A dummy job_config.yml, system_config.yml, and user_config.yml files.
+	- The Load_configs.yml file exemplifying the use of argparse library.
+	- The Live_session_Jan3.ipynb file containing the Github API code with data visuals for the top most starred Github repos. 
